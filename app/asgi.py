50118a751a4d6aef7c2f4acc58e0aa7f681df5c1
raghavjaistra <raghav.jain@aistra.com>
2025-06-12 00:00:34 +0530

    core logic and heartbeat mechanism updated

--- app/asgi.py
+++ app/asgi.py
@@ -1,11 +1,32 @@
 import os
+import asyncio
+import datetime
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.layers import get_channel_layer
 from app.chat import routing as chat_routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
+HEARTBEAT_SECS= 10
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': URLRouter(chat_routing.websocket_urlpatterns),
+    'websocket': URLRouter(chat_routing.websocket_urlpatterns)
 })
+
+# heartbeat
+async def _heartbeat_loop():
+    channel_layer = get_channel_layer()
+    while True:
+        ts = datetime.datetime.utcnow().isoformat() + "Z"
+        await channel_layer.group_send(
+            "heartbeat",
+            {"type": "heartbeat", "ts": ts}
+        )
+        await asyncio.sleep(HEARTBEAT_SECS)
+
+async def _startup():
+    await asyncio.sleep(1)
+    asyncio.create_task(_heartbeat_loop())
+
+asyncio.get_event_loop().create_task(_startup())
--- app/chat/consumers.py
+++ app/chat/consumers.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from channels.generic.websocket import AsyncWebsocketConsumer
 
@@ -7,10 +8,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         logger.info('ws_connect')
         await self.accept()
+    
+    async def connect(self):
+        self.counter = 0
+        await self.channel_layer.group_add("heartbeat", self.channel_name)
+        logger.info("ws_connect", extra={"connection": self.channel_name})
+        await self.accept()
 
     async def receive(self, text_data):
-        logger.info('ws_message', extra={'payload': text_data})
-        await self.send(text_data=text_data)
+        self.counter += 1
+        logger.info("ws_message_received", extra={"count": self.counter})
+        await self.send(text_data=json.dumps({"count": self.counter}))
 
     async def disconnect(self, close_code):
-        logger.info('ws_disconnect', extra={'code': close_code})
+        await self.send(text_data=json.dumps({
+            "bye": True,
+            "total": self.counter
+        }))
+        await self.channel_layer.group_discard("heartbeat", self.channel_name)
+        logger.info("ws_disconnect", extra={"code": close_code})
+
+    async def heartbeat(self, event):
+        await self.send(text_data=json.dumps({
+            "ts": event["ts"]
+        }))
